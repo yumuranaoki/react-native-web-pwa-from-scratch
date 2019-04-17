df562eba23408040b218cc949bc49b9b2a11f4a8
yumuranaoki <naoki.yumura@gmail.com>
2019-04-18 08:40:09 +0900

    add caching logic

--- src/serviceWorker/index.ts
+++ src/serviceWorker/index.ts
@@ -1,6 +1,5 @@
 export const register = () => {
   if ('serviceWorker' in navigator) {
-    console.log('you can use service worker')
     navigator.serviceWorker.register('sw.js')
     .then(reg => {
       console.log(reg)
--- src/serviceWorker/sw.ts
+++ src/serviceWorker/sw.ts
@@ -1,5 +1,18 @@
+const cacheName = 'kg-cache';
+const filesToCache = [
+  'index.html',
+  '/bundle.js',
+];
+
 self.addEventListener('install', event => {
   console.log('installed')
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(cache => {
+      console.log(cache)
+      return cache.addAll(filesToCache);
+    })
+  )
 })
 
 self.addEventListener('activate', event => {
@@ -8,4 +21,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.log('fetch')
+  console.log(event)
+  console.log(caches)
+  // urlがcacheにマッチすればキャッシュから返す
 })
